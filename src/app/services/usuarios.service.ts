062b891b2e6ff8686c65fa4adf5debef6fa19850
Diego Pagini <dq@qbkconsulting.com>
2021-02-07 13:29:53 -0300

    header en peticion

--- src/app/services/usuarios.service.ts
+++ src/app/services/usuarios.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpParams } from '@angular/common/http';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root',
@@ -8,9 +8,16 @@ export class UsuariosService {
   constructor(private http: HttpClient) {}
 
   obtenerUsuario() {
-    const params = new HttpParams().append('page', '1');
+    let params = new HttpParams().append('page', '1');
+    params = params.append('nombre', 'Diego Pagini');
+
+    const headers = new HttpHeaders({
+      'token-usuario': 'ABC123ASD456',
+    });
+
     return this.http.get(`https://reqres.in/api/user`, {
       params: params,
+      headers: headers,
     });
   }
 }
